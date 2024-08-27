0ef0bcc611642d06accd751c23ae7f7a14ee7e48
brichards64 <brichards64>
2024-08-27 23:30:31 +0000

    some updates and the begging of an example

--- UserTools/Factory/Factory.cpp
+++ UserTools/Factory/Factory.cpp
@@ -1,12 +1,11 @@
 #include "Factory.h"
 #include "Unity.h"
 
-Tool* Factory(std::string tool){
+Tool* Factory(std::string tool) {
 Tool* ret=0;
 
 // if (tool=="Type") tool=new Type;
 if (tool=="DummyTool") ret=new DummyTool;
-
+if (tool=="Example") ret=new Example;
 return ret;
 }
-
